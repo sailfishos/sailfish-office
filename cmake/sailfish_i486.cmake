b4e64f64ce1f13f9b677f577cbdafc2fbbffa74a
Arjen Hiemstra <ahiemstra@heimr.nl>
2013-03-11 18:56:43 +0100

    Move deployment to toolchain file to make it possible to change it
    This makes it easier to later on do the same for deploying to devices

--- cmake/sailfish_i486.cmake
+++ cmake/sailfish_i486.cmake
@@ -5,6 +5,7 @@ set(CMAKE_C_COMPILER $ENV{HOME}/SailfishOS/share/qtcreator/MerProject/mer-sdk-to
 set(CMAKE_CXX_COMPILER $ENV{HOME}/SailfishOS/share/qtcreator/MerProject/mer-sdk-tools/MerSDK/SailfishOS-i486-x86/gcc)
 
 set(CMAKE_FIND_ROOT_PATH $ENV{HOME}/SailfishOS/mersdk/targets/SailfishOS-i486-x86/)
-#link_directories($ENV{HOME}/SailfishOS/mersdk/targets/SailfishOS-i486-x86/usr/lib/)
 
 set(QT_QMAKE_EXECUTABLE $ENV{HOME}/SailfishOS/mersdk/targets/SailfishOS-i486-x86/usr/bin/qmake)
+
+add_custom_target(deploy ${CMAKE_SOURCE_DIR}/deploy/deploy_emulator.sh COMMENT "Deploying to emulator...")
