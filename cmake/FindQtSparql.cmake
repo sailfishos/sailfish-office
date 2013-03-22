1682c0b3ee7a30b7fd047e7586b5b8fcf533d107
Arjen Hiemstra <ahiemstra@heimr.nl>
2013-03-22 13:35:13 +0100

    Fix the FindQtSparql.cmake to find things outside /usr

--- cmake/FindQtSparql.cmake
+++ cmake/FindQtSparql.cmake
@@ -1,17 +1,15 @@
-if(SAILFISH_SCRATCHBOX)
-    set(QTSPARQL_INCLUDES_DIR /usr/include/QtSparql)
-    message("!! Assuming QtSparql in /usr/include/QtSparql - Ensure libqtsparql-devel is installed in the Sailfish SDK scratchbox target, as well as libqtsparql and libqtsparql-tracker on the runtime VM")
-else()
-    find_path(QTSPARQL_INCLUDES_DIR
-        QtSparql
-        PATHS
-        /usr/include
-        /usr/include/QtSparql
-        /usr/include/qt4/QtSparql
-    )
+find_path(QTSPARQL_INCLUDE_DIR
+    QtSparql
+    PATH_SUFFIXES
+    QtSparql
+    qt4/QtSparql
+)
 
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(QtSparql DEFAULT_MSG QTSPARQL_INCLUDES_DIR)
-endif(SAILFISH_SCRATCHBOX)
+find_library(QTSPARQL_LIBRARY
+    QtSparql
+)
 
-mark_as_advanced(QTSPARQL_INCLUDES_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(QtSparql DEFAULT_MSG QTSPARQL_INCLUDE_DIR QTSPARQL_LIBRARY)
+
+mark_as_advanced(QTSPARQL_INCLUDE_DIR QTSPARQL_LIBRARY)
