d776759de55e26fda76277e04eac71f71bccf74b
Dan Leinir Turthra Jensen <admin@leinir.dk>
2013-04-03 10:15:23 +0100

    [office] Add a jolla signon service cmake finder and use it. Contributes to JB#981

---
+++ cmake/FindJollaSignonUiService.cmake
@@ -0,0 +1,14 @@
+find_path(JOLLASIGNONUISERVICE_INCLUDE_DIR
+    signonuiservice.h
+    PATH_SUFFIXES
+    libjollasignonuiservice
+)
+
+find_library(JOLLASIGNONUISERVICE_LIBRARY
+    libjollasignonuiservice
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(libjollasignonuiservice DEFAULT_MSG JOLLASIGNONUISERVICE_INCLUDE_DIR JOLLASIGNONUISERVICE_LIBRARY)
+
+mark_as_advanced(JOLLASIGNONUISERVICE_INCLUDE_DIR JOLLASIGNONUISERVICE_LIBRARY)
