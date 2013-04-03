8b8e793c2048085bf6d97bf8e1e3937cfad986b0
Dan Leinir Turthra Jensen <admin@leinir.dk>
2013-04-03 13:58:19 +0100

    [office] Reinstate the code for the accounts object registration. Contributes to JB#981

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <QDeclarativeView>
 
 #include <QtDBus/QDBusConnection>
-// #include <libjollasignonuiservice/signonuiservice.h>
+#include <libjollasignonuiservice/signonuiservice.h>
 
 #include "sailfishapplication/sailfishapplication.h"
 #include "models/documentlistmodel.h"
@@ -15,31 +15,31 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     auto view = Sailfish::createView("main.qml");
 
     // We want to have SignonUI in process, if user wants to create account from Documents
-//     SignonUiService *ssoui = new SignonUiService(0, true); // in process
-//     ssoui->setInProcessServiceName(QLatin1String("com.jolla.documents"));
-//     ssoui->setInProcessObjectPath(QLatin1String("/JollaDocumentsSignonUi"));
-// 
-//     QDBusConnection sessionBus = QDBusConnection::sessionBus();
-//     bool registeredService = sessionBus.registerService(QLatin1String("com.jolla.gallery"));
-//     bool registeredObject = sessionBus.registerObject(QLatin1String("/JollaGallerySignonUi"), ssoui,
-//             QDBusConnection::ExportAllContents);
-// 
-//     if (!registeredService || !registeredObject) {
-//         qWarning() << Q_FUNC_INFO << "CRITICAL: unable to register signon ui service:"
-//                    << QLatin1String("com.jolla.gallery") << "at object path:"
-//                    << QLatin1String("/JollaGallerySignonUi");
-//     }
-// 
-//     view->rootContext()->setContextProperty("jolla_signon_ui_service", ssoui);
+    SignonUiService *ssoui = new SignonUiService(0, true); // in process
+    ssoui->setInProcessServiceName(QLatin1String("org.sailfish.documents"));
+    ssoui->setInProcessObjectPath(QLatin1String("/SailfishDocumentsSignonUi"));
+
+    QDBusConnection sessionBus = QDBusConnection::sessionBus();
+    bool registeredService = sessionBus.registerService(QLatin1String("org.sailfish.gallery"));
+    bool registeredObject = sessionBus.registerObject(QLatin1String("/SailfishDocumentsSignonUi"), ssoui,
+            QDBusConnection::ExportAllContents);
+
+    if (!registeredService || !registeredObject) {
+        qWarning() << Q_FUNC_INFO << "CRITICAL: unable to register signon ui service:"
+                   << QLatin1String("org.sailfish.documents") << "at object path:"
+                   << QLatin1String("/SailfishDocumentsSignonUi");
+    }
+
+    view->rootContext()->setContextProperty("jolla_signon_ui_service", ssoui);
 
     Sailfish::showView(view);
 
     int retn = app->exec();
 
-//     if (registeredService)
-//         sessionBus.unregisterService(QLatin1String("com.jolla.documents"));
-//     if (registeredObject)
-//         sessionBus.unregisterObject(QLatin1String("/JollaDocumentsSignonUi"));
-//     delete ssoui;
+    if (registeredService)
+        sessionBus.unregisterService(QLatin1String("org.sailfish.documents"));
+    if (registeredObject)
+        sessionBus.unregisterObject(QLatin1String("/SailfishDocumentsSignonUi"));
+    delete ssoui;
     return retn;
 }
