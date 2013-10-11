5b42f8183dd76c2abf021ffc078b90040d0b80d4
Dan Leinir Turthra Jensen <admin@leinir.dk>
2013-10-11 13:58:58 +0100

    [office] Adapt toolchain file to new file locations (but now merssh complains). Contributes to JB#981

--- cmake/sailfish_i486.cmake
+++ cmake/sailfish_i486.cmake
@@ -3,24 +3,24 @@ set(SAILFISH_SCRATCHBOX 1)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 
-set(CMAKE_C_COMPILER $ENV{HOME}/SailfishOS/share/qtcreator/MerProject/mer-sdk-tools/MerSDK/SailfishOS-i486-x86/gcc)
-set(CMAKE_CXX_COMPILER $ENV{HOME}/SailfishOS/share/qtcreator/MerProject/mer-sdk-tools/MerSDK/SailfishOS-i486-x86/gcc)
+set(CMAKE_C_COMPILER $ENV{HOME}/.config/SailfishAlpha/mer-sdk-tools/MerSDK/SailfishOS-i486-x86/gcc)
+set(CMAKE_CXX_COMPILER $ENV{HOME}/.config/SailfishAlpha/mer-sdk-tools/MerSDK/SailfishOS-i486-x86/gcc)
 
 set(CMAKE_FIND_ROOT_PATH $ENV{HOME}/SailfishOS/mersdk/targets/SailfishOS-i486-x86/)
 
-set(QT_QMAKE_EXECUTABLE $ENV{HOME}/SailfishOS/mersdk/targets/SailfishOS-i486-x86/usr/lib/qt4/bin/qmake)
+set(QT_QMAKE_EXECUTABLE $ENV{HOME}/.config/SailfishAlpha/mer-sdk-tools/MerSDK/SailfishOS-i486-x86/qmake)
 
 add_custom_target(deploy COMMENT "Deploying to emulator...")
 add_custom_command(TARGET deploy POST_BUILD COMMAND mkdir ARGS -p deploy)
 add_custom_command(TARGET deploy POST_BUILD COMMAND make ARGS DESTDIR=deploy install)
-add_custom_command(TARGET deploy POST_BUILD COMMAND scp ARGS -r -P 2223 -i $ENV{HOME}/.ssh/mer-qt-creator-rsa deploy/* root@localhost:/)
+add_custom_command(TARGET deploy POST_BUILD COMMAND scp ARGS -r -P 2223 -i $ENV{HOME}/SailfishOS/vmshare/ssh/private_keys/1/root deploy/* root@localhost:/)
 add_custom_command(TARGET deploy POST_BUILD COMMAND rm ARGS -rf deploy)
 add_dependencies(deploy all)
 
 add_custom_target(run COMMENT "Running application...")
-add_custom_command(TARGET run POST_BUILD COMMAND ssh ARGS -p 2223 -i $ENV{HOME}/.ssh/mer-qt-creator-rsa nemo@localhost "/usr/bin/invoker --type=j -s /usr/bin/sailfish-office")
+add_custom_command(TARGET run POST_BUILD COMMAND ssh ARGS -p 2223 -i $ENV{HOME}/SailfishOS/vmshare/ssh/private_keys/1/nemo nemo@localhost "/usr/bin/invoker --type=j -s /usr/bin/sailfish-office")
 add_dependencies(run deploy)
 
 add_custom_target(debug COMMENT "Debugging application...")
-add_custom_command(TARGET debug POST_BUILD COMMAND ssh ARGS -p 2223 -i $ENV{HOME}/.ssh/mer-qt-creator-rsa nemo@localhost 'gdb -ex "run" /usr/bin/sailfish-office')
+add_custom_command(TARGET debug POST_BUILD COMMAND ssh ARGS -p 2223 -i $ENV{HOME}/SailfishOS/vmshare/ssh/private_keys/1/nemo nemo@localhost 'gdb -ex "run" /usr/bin/sailfish-office')
 add_dependencies(debug deploy)
