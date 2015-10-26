d06bf43d1cbe6ea078a5cecbef6cda04b324a0a5
Pekka Vuorela <pekka.vuorela@jollamobile.com>
2015-10-26 15:43:28 +0200

    Merge sailfishapplication into main.cpp
    Generic Sailfish application makes sense only if it's copy-pasted
    boilerplate code. That's not the case here.

--- main.cpp
+++ main.cpp
@@ -18,12 +18,70 @@
 
 #include <QApplication>
 #include <QGuiApplication>
-#include <QProcess>
+#include <QtQml>
 #include <QQuickView>
 #include <QQmlError>
 #include <QQmlContext>
 #include <QQmlEngine>
-#include "sailfishapplication.h"
+#include <QDBusConnection>
+#include <QTranslator>
+#include <QLocale>
+#include <QDebug>
+
+#include <MDeclarativeCache>
+
+#include "config.h"
+#include "models/filtermodel.h"
+#include "models/documentlistmodel.h"
+#include "models/trackerdocumentprovider.h"
+#include "models/documentproviderplugin.h"
+#include "models/documentproviderlistmodel.h"
+#include "dbusadaptor.h"
+
+namespace {
+QSharedPointer<QApplication> createApplication(int &argc, char **argv)
+{
+    auto app = QSharedPointer<QApplication>(new QApplication(argc, argv));
+    //FIXME: We should be able to use a pure QGuiApplication but currently too much of
+    //Calligra depends on QApplication.
+    //QSharedPointer<QGuiApplication>(MDeclarativeCache::qApplication(argc, argv));
+
+    QTranslator *engineeringEnglish = new QTranslator(app.data());
+    if (!engineeringEnglish->load("sailfish-office_eng_en", TRANSLATION_INSTALL_DIR))
+        qWarning("Could not load engineering english translation file!");
+    QCoreApplication::installTranslator(engineeringEnglish);
+
+    QTranslator *translator = new QTranslator(app.data());
+    if (!translator->load(QLocale::system(), "sailfish-office", "-", TRANSLATION_INSTALL_DIR))
+        qWarning(("Could not load translations for " + QLocale::system().name()).toLatin1());
+    QCoreApplication::installTranslator( translator );
+
+    return app;
+}
+
+QSharedPointer<QQuickView> createView(const QString &file)
+{
+    qmlRegisterType<DocumentListModel>("Sailfish.Office.Files", 1, 0, "DocumentListModel");
+    qmlRegisterType<DocumentProviderListModel>("Sailfish.Office.Files", 1, 0, "DocumentProviderListModel");
+    qmlRegisterType<TrackerDocumentProvider>("Sailfish.Office.Files", 1, 0, "TrackerDocumentProvider");
+    qmlRegisterType<FilterModel>("Sailfish.Office.Files", 1, 0, "FilterModel");
+    qmlRegisterInterface<DocumentProviderPlugin>("DocumentProviderPlugin");
+
+    QSharedPointer<QQuickView> view(MDeclarativeCache::qQuickView());
+    view->engine()->addImportPath(CALLIGRA_QML_PLUGIN_DIR);
+    view->setSource(QUrl::fromLocalFile(QML_INSTALL_DIR + file));
+
+    new DBusAdaptor(view.data());
+
+    if (!QDBusConnection::sessionBus().registerObject("/org/sailfish/office/ui", view.data()))
+        qWarning() << "Could not register /org/sailfish/office/ui D-Bus object.";
+
+    if (!QDBusConnection::sessionBus().registerService("org.sailfish.office"))
+        qWarning() << "Could not register org.sailfish.office D-Bus service.";
+
+    return view;
+}
+}
 
 class CoverWindowFetcher : public QObject
 {
@@ -49,13 +107,13 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     // TODO: start using Silica booster
     QQuickWindow::setDefaultAlphaBuffer(true);
 
-    auto app = Sailfish::createApplication(argc, argv);
+    auto app = createApplication(argc, argv);
     // Note, these must be said now, otherwise some plugins using QSettings
     // will get terribly confused when they fail to load properly.
     app->setOrganizationName("Sailfish");
     app->setApplicationName("Sailfish Office");
 
-    auto view = Sailfish::createView("Main.qml");
+    auto view = createView("Main.qml");
 
     QQmlContext *context = view->rootContext();
     context->setContextProperty(QStringLiteral("coverWindowAccessor"), new CoverWindowFetcher(view->engine()));
@@ -66,7 +124,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     int retn = 1;
     if (!view->errors().count() > 0) {
-        Sailfish::showView(view);
+        view->showFullScreen();
         retn = app->exec();
     }
 
